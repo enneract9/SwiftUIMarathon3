d975d27dcf9ad55319e0885c9587bc228473dd0e
enneract9 <vargunin.iv@gmail.com>
2024-12-07 15:02:41 +0500

    Initial Commit

---
+++ SwiftUIMarathon3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIMarathon3
+//
+//  Created by @_@ on 07.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIMarathon3/SwiftUIMarathon3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMarathon3App.swift
+//  SwiftUIMarathon3
+//
+//  Created by @_@ on 07.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMarathon3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
